8fd13dbaa55863a3b15db21f2f0ec5c56387a997
bhanuteja24 <bhanuteja24@gmail.com>
2016-06-16 15:56:50 -0500

    Edited Front End

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ActiveRecord::Base
 	validates :name, presence: :true
-	validates :phone_number, presence: :true, length: { is: 6 }
+	validates :phone_number, presence: :true, length: { is: 10 }
 	validates :party_size, :date, presence: :true
 	validates :time, presence: :true, uniqueness: true
 	validates :party_size, presence: :true
